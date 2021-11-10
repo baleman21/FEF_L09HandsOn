24adbcbf38aa35254e9228c667ed40065e1f798e
Bryana Aleman <92816637+baleman21@users.noreply.github.com>
2021-11-10 14:27:21 -0600

    Add files via upload

---
+++ L09HandsOn/script.js
@@ -0,0 +1,23 @@
+let newRequest = new XMLHttpRequest();
+newRequest.onreadystatechange = function() {
+    if(this.readyState == 4 && this.status == 200) {
+        let myObj = JSON.parse(this.responseText);
+        document.getElementById("name").innerHTML = myObj.name;
+        document.getElementById("birthday").innerHTML = myObj.birthday;
+    }
+}
+
+newRequest.open("GET", "einstein.json", true);
+newRequest.send();
+
+function myFunction() {
+    newRequest.onreadystatechange = function() {
+        if(this.readyState == 4 && this.status == 200) {
+            let myObj = JSON.parse(this.responseText);
+            document.getElementById("bio").innerHTML = myObj.bio;
+        }
+    }
+    
+    newRequest.open("GET", "einstein.json", true);
+    newRequest.send();
+}
